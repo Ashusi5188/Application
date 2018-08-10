a302774317ac995cf8c92da8554e49d3ff427b3a
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-08-10 22:42:07 +0530

    Update TestRunner.java
    commented

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -3,10 +3,21 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(
+/*@CucumberOptions(
         features = "src/test/resources/features"
         ,glue={"stepdefs"},
         tags = {"@TC001"}
+)*/
+@CucumberOptions(features = "src/test/resources/features", // Test scenarios
+        glue = {"cucumber.stepdefs"}, // Steps definitions
+        format = {"pretty", // Cucumber report formats and location to store them in phone/emulator
+                "html:/mnt/sdcard/cucumber-reports/cucumber-html-report",
+                "json:/mnt/sdcard/cucumber-reports/cucumber.json",
+                "junit:/mnt/sdcard/cucumber-reports/cucumber.xml"
+            // Note: if you don't have write access to /mnt/sdcard/ on the phone use instead
+            // the following path here and in the build.gradle: /data/data/com.neoranga55.cleanguitestarchitecture/cucumber-reports/
+        },
+        tags={"@TC001"}
 )
 /*@CucumberOptions(features = {"src/test/resources/features"},
         glue={"src/test/java/stepdefs"},
