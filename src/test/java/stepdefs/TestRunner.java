07e568b48b2fe872089cbda399bff703a25f608f
Ashutosh <AshutoshKumar.singh@clarivate.com>
2018-08-10 20:31:05 +0530

    commiting files to git.

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -3,9 +3,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"},
+@CucumberOptions(
+        features = "src/test/resources/features"
+        ,glue={"stepdefs"},
+        tags = {"@TC001"}
+)
+/*@CucumberOptions(features = {"src/test/resources/features"},
         glue={"src/test/java/stepdefs"},
         tags = {"@TC001"},
-        monochrome = true)
+        monochrome = true)*/
 public class TestRunner {
 }
