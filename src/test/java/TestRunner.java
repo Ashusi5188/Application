c8640d013dd8f37782ee8327994467c637927ebe
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-08-12 00:04:12 +0530

    Update TestRunner.java
    b

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         tags = {"@TC001"},
-         glue = {"src/test/java/stepdefs"}
+         glue = {"src.test.java.stepdefs"}
         
 )
 /*@CucumberOptions(features = "src/test/resources/features", // Test scenarios
