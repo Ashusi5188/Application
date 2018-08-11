e61b2bc279574f2cc0fb4ec20e91a34a2afe5df9
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-08-11 22:45:26 +0530

    Update TestRunner.java
    ss

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -3,15 +3,14 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-/*@CucumberOptions(
-        features = "src/test/resources/features"
-        ,glue={"stepdefs"},
+@CucumberOptions(
+        features = "src/test/resources/features",
         tags = {"@TC001"}
-)*/
-@CucumberOptions(features = "src/test/resources/features", // Test scenarios
+)
+/*@CucumberOptions(features = "src/test/resources/features", // Test scenarios
         glue = {"stepdefs"}, // Steps definitions
         tags={"@TC001"}
-)
+)*/
 /*@CucumberOptions(features = {"src/test/resources/features"},
         glue={"src/test/java/stepdefs"},
         tags = {"@TC001"},
