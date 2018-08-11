107c03faa2d27d81b4f3c39484728c0f36adac17
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-08-11 23:34:07 +0530

    Add files via upload
    aaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,20 @@
+package stepdefs;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+         glue = {"stepdefs"},
+        tags = {"@TC001"}
+)
+/*@CucumberOptions(features = "src/test/resources/features", // Test scenarios
+        glue = {"stepdefs"}, // Steps definitions
+        tags={"@TC001"}
+)*/
+/*@CucumberOptions(features = {"src/test/resources/features"},
+        glue={"src/test/java/stepdefs"},
+        tags = {"@TC001"},
+        monochrome = true)*/
+public class TestRunner {
+}
