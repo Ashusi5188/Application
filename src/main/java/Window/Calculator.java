8acee199832e4248d0321aafb85d9f185e4f2c96
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-08-13 19:48:22 +0530

    Update Calculator.java
    dddddd

--- src/main/java/Window/Calculator.java
+++ src/main/java/Window/Calculator.java
@@ -3,7 +3,7 @@ package Window;
 public class Calculator extends AppBase {
     public Calculator()  {
         //control.run("C:\\Program Files (x86)\\Sicyon calculator\\SicyonCalc.exe");
-        //control.run("C:\\Windows\\System32\\calc.exe");
+        control.run("C:\\Windows\\System32\\calc.exe");
       control.winActivate("Calculator");
        control.winWaitActive("Calculator");
     }
