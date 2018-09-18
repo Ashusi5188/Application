c8e3db651909f2473ef4fe8935adc9dd8843316b
Ashusi5188 <37295111+Ashusi5188@users.noreply.github.com>
2018-09-18 17:23:28 +0530

    Update TestRunner.java
    ffff

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -24,26 +24,3 @@ public class TestRunner {
         Reporter.setTestRunnerOutput("Sample test runner output message");
     }
 }
-=======
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        tags = {"@TC001"},
-         glue = {"stepdefs"},
-        monochrome = true
-        
-)
-/*@CucumberOptions(features = "src/test/resources/features", // Test scenarios
-        glue = {"stepdefs"}, // Steps definitions
-        tags={"@TC001"}
-)*/
-/*@CucumberOptions(features = {"src/test/resources/features"},
-        glue={"src/test/java/stepdefs"},
-        tags = {"@TC001"},
-        monochrome = true)*/
-public class TestRunner {
-}
->>>>>>> dc88d52b3ac7f07664bd1cc2d5a923bcecccdd04:src/test/java/TestRunner.java
