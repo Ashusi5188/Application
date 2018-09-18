048620ece91ae8bf43261eec72d193b7f5f4d477
Ashutosh <AshutoshKumar.singh@clarivate.com>
2018-09-18 17:04:58 +0530

    commiting files to git.

--- src/test/java/stepdefs/CalculatorStepDefinition.java
+++ src/test/java/stepdefs/CalculatorStepDefinition.java
@@ -1,31 +1,71 @@
 package stepdefs;
 
-import Window.AppBase;
 import Window.Calculator;
-import cucumber.api.PendingException;
+import com.cucumber.listener.Reporter;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class CalculatorStepDefinition {
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
-    Calculator calc =calc= new Calculator();
+public class CalculatorStepDefinition {
+    @Before(" @TC001")
+    public void setup() {
+        System.out.println("open");
+    }
+
+    @After("@TC001")
+    public static void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                System.out.println("Test case which got failed is " + scenario.getName());
+                Robot awt_robot = new Robot();
+                BufferedImage Entire_Screen = awt_robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                String Destination = "./ScreenShots/Entire_Screen.png";
+                ImageIO.write(Entire_Screen, "PNG", new File(Destination));
+                Reporter.addScreenCaptureFromPath("." + Destination);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                System.out.println("Test case which got passed is " + scenario.getName());
+                Robot awt_robot = new Robot();
+                BufferedImage Entire_Screen = awt_robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                String Destination = "./ScreenShots/Entire_Screen.png";
+                ImageIO.write(Entire_Screen, "PNG", new File(Destination));
+                Reporter.addScreenCaptureFromPath("." + Destination);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Calculator calc = calc = new Calculator();
+
     @Given("^user opens calculator application$")
-    public void userOpensCalculatorApplication() {
+    public void userOpensCalculatorApplication() throws InterruptedException {
 
     }
 
     @When("^User enter first number$")
-    public void userEnterFirstNumber()  {
+    public void userEnterFirstNumber() {
 
-            calc.enter3();
+        calc.enter3();
 
 
     }
 
     @And("^User enter plus operator$")
-    public void userEnterPlusOperator()  {
+    public void userEnterPlusOperator() {
         try {
             calc.enterOperator();
         } catch (InterruptedException e) {
@@ -34,7 +74,7 @@ public class CalculatorStepDefinition {
     }
 
     @And("^User enters second number$")
-    public void userEntersSecondNumber()  {
+    public void userEntersSecondNumber() {
         try {
             calc.enter9();
         } catch (InterruptedException e) {
@@ -46,7 +86,7 @@ public class CalculatorStepDefinition {
     public void userGetsResult() {
         try {
             calc.enterEqual();
-           calc.close();
+            calc.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -55,4 +95,5 @@ public class CalculatorStepDefinition {
 
 
 }
+
 
